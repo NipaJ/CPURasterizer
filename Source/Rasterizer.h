64485e6045641c0f5f3dd3d1342a42287bd4fa1a
Niilo Jaba <niilo.jaba@gmail.com>
2015-11-10 05:27:08 +0200

    [WIP] SIMD optimizations for the pixel rasterizer
    Trying to rasterize 4x2 blocks.

--- Source/Rasterizer.h
+++ Source/Rasterizer.h
@@ -22,24 +22,66 @@ namespace nmj
 
 	/**
 	 * Rasterizer output data.
-	 *
-	 * Buffers should point to an array with size of (width * height).
 	 */
 	struct RasterizerOutput
 	{
 		/**
-		 * 32bit RGBA color buffer.
-		 * 16 byte alignment required.
+		 * 8bit channel RGB color buffer.
+		 *
+		 * Buffer should be rounded up to size that matches the 4x2 block
+		 * layout and 16 byte alignment is required.
+		 *
+		 * The memory is packed as 4x2 pixels block in following layout:
+		 * 00: R1 R2 R3 R4 R5 R6 R7 R8
+		 * 08: G1 G2 G3 G4 G5 G6 G7 G8
+		 * 16: B1 B2 B3 B4 B5 B6 B7 B8
+		 * 24: X  X  X  X  X  X  X  X
+		 *
+		 * From screen-space layout of:
+		 *  -------------------- X
+		 * | RGB1 RGB2 RGB3 RGB4
+		 * | RGB5 RGB6 RGB7 RGB8
+		 * Y
+		 *
+		 * You can access block of pixels like this:
+		 * U8 *block = (U8 *)color_buffer;
+		 * block += color_pitch * (y / 2);
+		 * block += (x / 4) * 32;
 		 */
-		U32 *color_buffer;
+		void *color_buffer;
 
 		/**
 		 * 16bit unsigned normalized depth buffer.
-		 * 16 byte alignment required.
+		 *
+		 * Buffer should be rounded up to size that matches the 4x2 block
+		 * layout and 16 byte alignment is required.
+		 *
+		 * The memory is packed as 4x2 pixels block in following layout:
+		 * 00: D1 D2 D3 D4 
+		 * 08: D5 D6 D7 D8
+		 *
+		 * From screen-space layout of:
+		 *  ------------ X
+		 * | D1 D2 D3 D4 
+		 * | D5 D6 D7 D8
+		 * Y
+		 *
+		 * You can access block of pixels like this:
+		 * U8 *block = (U8 *)depth_buffer;
+		 * block += depth_pitch * (y / 2);
+		 * block += (x / 4) * 16;
 		 */
-		U16 *depth_buffer;
+		void *depth_buffer;
 
-		/* Output resolution. */
+		/**
+		 * Byte offset to step one row of 4x2 blocks.
+		 */
+		U32 color_pitch;
+		U32 depth_pitch;
+
+		/**
+		 * Output resolution.
+		 */
 		U16 width, height;
 	};
 
